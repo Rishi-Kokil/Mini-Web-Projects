029275420ca3663276daddb9ca59ed997062334c
Rishi Kokil <rishikokil@gmail.com>
2023-12-26 12:45:01 +0530

    made Chanegs to table generating program

--- Table Generating program/index.js
+++ Table Generating program/index.js
@@ -1,7 +1,13 @@
+let divTableContainer = document.querySelector(".tableContainer");
+
 let numberOfColumns = document.querySelector("#noOfColumns");
 let numberOfRows = document.querySelector("#noOfRows");
 let rowCount = 0;
 let columnCount = 0;
+
+let table  = document.createElement("table");
+table.innerHTML = " ";
+divTableContainer.appendChild(table);
 
 
 //-------------------------------------------------
@@ -34,3 +40,37 @@ numberOfRows.addEventListener("input" , ()=>{
 // numberOfRows.addEventListener("blur" , ()=>{
 //     numberOfRows.value = "Rows";
 // });
+
+//-------------------------------------
+let enterBtn = document.querySelector("#enter");
+enterBtn.addEventListener("click" , ()=>{
+    table.innerHTML = " ";
+    // coverting string to number using the plus operator
+    if(+rowCount != 0 && +columnCount != 0){
+        for(let i = 0 ; i < rowCount ; i++){
+            let row = document.createElement("tr");
+
+            for(let j = 0 ; j < columnCount ; j++){
+                if(i === 0){
+                    let cell = document.createElement("th");
+                    let inputBox = document.createElement("input");
+                    inputBox.type = "text";
+                    inputBox.name = `${i}${j}cell`;
+                    inputBox.id = `${i}${j}cell`
+                    cell.appendChild(inputBox);
+                    row.appendChild(cell);
+                }
+                else{
+                    let cell = document.createElement("td");
+                    let inputBox = document.createElement("input");
+                    inputBox.type = "text";
+                    inputBox.name = `${i}${j}cell`;
+                    inputBox.id = `${i}${j}cell`
+                    cell.appendChild(inputBox);
+                    row.appendChild(cell);
+                }
+            }
+            table.append(row);
+        }
+    }
+});
